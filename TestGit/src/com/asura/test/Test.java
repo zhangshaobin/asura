98165753ba43683be4c3b5c8a404787f609dfc27
Administrator <Administrator@PC201410141040>
2014-10-15 09:46:45 +0800

    111

---
+++ TestGit/src/com/asura/test/Test.java
@@ -0,0 +1,15 @@
+package com.asura.test;
+
+public class Test {
+	
+	/**
+	 * git ≤‚ ‘
+	 * 
+	 * @param args
+	 */
+	public static void main(String []args) {
+		
+		System.out.println("hello world");
+	}
+
+}
